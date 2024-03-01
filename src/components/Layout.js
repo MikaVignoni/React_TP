cfe1e02e31426ad0360c3622342889f1fb6bac0b
MikaVignoni <md.vignoni@gmail.com>
2024-03-01 18:47:54 +0100

    Global Styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,8 @@ export default function Layout({title,children}) {
         <meta name="description" content="Huellitas" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        
+        
       </Head>
       <main>
         <NavBar />
