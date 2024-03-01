65b8d2c220d3e48f7ff0723ac438fc5ed0776267
Noec84 <noecmontori@gmail.com>
2024-03-01 19:19:06 -0300

    Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,4 @@
 "use client"
-
 import { useState } from 'react';
 import styles from "../styles/Home.module.css";
 
@@ -10,88 +9,42 @@ const Formulario = () => {
         email: '',
         mensaje: ''
     });
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
     };
 
     const handleChange = (e) => {
-        setFormData(form =>{
-            return {
-                ...form,
-                [e.target.name]: e.target.value
-            }
-        })
-        
-    }
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [e.target.name]: e.target.value
+        }));
+    };
+
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="nombreApellido">Nombre y Apellido:</label>
-                <input type="text" id="nombreApellido" name="nombreApellido" value={formData.nombreApellido} onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="telefono">Teléfono de Contacto:</label>
-                <input type="tel" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="email">Mail de Contacto:</label>
-                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="mensaje">Mensaje:</label>
-                <textarea id="mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange} required/>
-            </div>
-            <button type="submit">ENVIAR</button>
-        </form>
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="nombreApellido">Nombre y Apellido:</label>
+                    <input type="text" id="nombreApellido" name="nombreApellido" value={formData.nombreApellido} onChange={handleChange} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="telefono">Teléfono de Contacto:</label>
+                    <input type="tel" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Mail de Contacto:</label>
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="mensaje">Mensaje:</label>
+                    <textarea id="mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange} required/>
+                </div>
+                <button type="submit">ENVIAR</button>
+            </form>
+        </div>
     )
 }
-<styles-jsx>
- .form-container {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    background-color: #f9f9f9;
-  }
-  
-  .form-group {
-    margin-bottom: 20px;
-  }
-  
-  label {
-    display: block;
-    font-weight: bold;
-  }
-  
-  input[type="text"],
-  input[type="tel"],
-  input[type="email"],
-  textarea {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-sizing: border-box;
-    margin-top: 5px;
-  }
-  
-  button {
-    width: 100%;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    background-color: #4CAF50;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #FEDD89;
-  }
-  
-  </styles-jsx>
 
-export default Formulario
+export default Formulario;
