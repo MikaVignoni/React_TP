ea45f46a5696b8a0fb5046a5f07027910a16ebc7
Noec84 <noecmontori@gmail.com>
2024-03-13 19:02:44 -0300

    cambios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,60 @@
-
-
+import Link from 'next/link';
+import Mail from './icons/Mail';
+import Facebook from './icons/Facebook';
+import Instagram from './icons/Instagram';
+import Whatsapp from './icons/Watsapp';
+import MapaPin from './icons/MapaPin';
 
 const Footer = () => {
   return ( 
   <>
-    <div className="Footer">
-      <h2>Footer </h2>
-    </div>
+    <footer>
+      <div className="container">
+        <div className='footer-links'>
+        <Link href="/" >
+              <img src="Huellitas_Icon.png" height="60px" title="huellita" >
+              </img>
+          </Link>       
+        </div>
 
+        <div className="footer-row">
+          <h4>Seguinos en redes:</h4> 
+        </div>
+        <div className="footer-row">   
+          <div className='footer-links'>
+            <div className='social-links'>
+                <Link href="facebook.com" >
+                  <Facebook/>
+                </Link>
+                <Link href="instagram.com" >
+                  <Instagram/>
+                </Link>
+                <Link href="whatsapp" >
+                  <Whatsapp/>
+                </Link>
+              </div>        
+          </div>
+        </div>
+        <div className="footer-row">
+        <Link href="mail" >
+          <Mail/>
+        </Link>
+          <h4>huellitas@refugio.com</h4> 
+        </div>
+        <div className="footer-row">
+        <Link href="mapapin" >
+            <MapaPin/>
+        </Link>
+          <h4>La Plata, Argentina</h4> 
+        </div>
+      </div>
+    </footer>
 
+  
 
     <style jsx> 
     {`
-      .Footer{
+      footer{
         background-color: var(--colorPrincipal);
       }
     `}
@@ -24,4 +66,4 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default Footer;
