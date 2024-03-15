6432a34715129b1eb049eae67a8f842e5414ee90
MikaVignoni <md.vignoni@gmail.com>
2024-03-15 13:44:55 +0100

    footer Styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,11 +13,12 @@ const Footer = () => {
       <div className="container">
         <div className='footer-links'>
           <Link href="/" >
-            <img src="Huellitas_Icon.png" height="60px" alt="huellita" />
+            <img src="Huellitas_Icon.png" height="60px" alt="huellita" id="Huella_Icon"/>
+            <img src="../img/Logotipo/Logotipo_huellitas_negro.png" alt="Logotipo Huellitas" id='Huellitas_Texto' />
           </Link>       
         </div>
 
-        <div className="footer-row">
+        <div className="social">
           <h4>Seguinos en redes:</h4> 
           <div className='footer-links'>
             <div className='social-links'>
@@ -56,19 +57,27 @@ const Footer = () => {
         }
         .container {
           display: flex;
-          justify-content: space-between;
-          align-items: center;
+          justify-content: space-around;
         }
         .footer-links img {
           display: block;
           height: 60px;
+          justify-content: center;
         }
-        .footer-row {
+        #Huellitas_Texto{display: none;}
+        .social {
           display: flex;
           align-items: center;
+          flex-direction: column;
         }
         .footer-row h4 {
           margin-left: 10px;
+        }
+
+        .footer-row{
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
         }
         .social-links {
           display: flex;
@@ -76,6 +85,15 @@ const Footer = () => {
         }
         .social-links > * {
           margin-right: 10px;
+        }
+
+        @media (min-width: 880px) {
+          .social {flex-direction: row;}
+        }
+
+        @media (min-width: 1300px) {
+          #Huellitas_Texto{display:inline; width: 150px; height: auto; transform: translate(0%, -50%)}
+          #Huella_Icon{height: 60px; width: auto;   transform: translate(0%, -35%)}
         }
 
       `}</style>
