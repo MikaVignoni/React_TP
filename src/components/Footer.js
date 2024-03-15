0b2940c7aeade785274a99ee472b4e6db7f77ac4
Noec84 <noecmontori@gmail.com>
2024-03-15 09:14:35 -0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,68 +3,84 @@ import Link from 'next/link';
 import Mail from './icons/Mail';
 import Facebook from './icons/Facebook';
 import Instagram from './icons/Instagram';
-import Whatsapp from './icons/Watsapp';
+import Whatsapp from './icons/Whatsapp';
 import MapaPin from './icons/MapaPin';
 
 const Footer = () => {
   return ( 
-  <>
+    <>
     <footer>
       <div className="container">
         <div className='footer-links'>
-        <Link href="/" >
-              <img src="Huellitas_Icon.png" height="60px" title="huellita" >
-              </img>
+          <Link href="/" >
+            <img src="Huellitas_Icon.png" height="60px" alt="huellita" />
           </Link>       
         </div>
 
         <div className="footer-row">
           <h4>Seguinos en redes:</h4> 
-        </div>
-        <div className="footer-row">   
           <div className='footer-links'>
             <div className='social-links'>
-                <Link href="facebook.com" >
-                  <Facebook/>
-                </Link>
-                <Link href="instagram.com" >
-                  <Instagram/>
-                </Link>
-                <Link href="whatsapp" >
-                  <Whatsapp/>
-                </Link>
-              </div>        
+              <Link href="https://facebook.com" >
+                <Facebook />
+              </Link>
+              <Link href="https://instagram.com" >
+                <Instagram />
+              </Link>
+              <Link href="https://whatsapp.com" >
+                <Whatsapp />
+              </Link>
           </div>
         </div>
+
         <div className="footer-row">
-        <Link href="mail" >
-          <Mail/>
-        </Link>
+          <Link href="mailto:huellitas@refugio.com" >
+            <Mail />
+          </Link>
           <h4>huellitas@refugio.com</h4> 
         </div>
+
         <div className="footer-row">
-        <Link href="mapapin" >
-            <MapaPin/>
-        </Link>
+          <Link href="https://mapapin.com" >
+            <MapaPin />
+          </Link>
           <h4>La Plata, Argentina</h4> 
         </div>
       </div>
-    </footer>
+      </div>
+</footer>
+      <style jsx>{`
+        footer {
+          background-color: var(--colorPrincipal);
+          padding: 20px;
+        }
+        .container {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .footer-links img {
+          display: block;
+          height: 60px;
+        }
+        .footer-row {
+          display: flex;
+          align-items: center;
+        }
+        .footer-row h4 {
+          margin-left: 10px;
+        }
+        .social-links {
+          display: flex;
+          align-items: center;
+        }
+        .social-links > * {
+          margin-right: 10px;
+        }
 
-  
-
-    <style jsx> 
-    {`
-      footer{
-        background-color: var(--colorPrincipal);
-      }
-    `}
-    </style>
-
-
-
-  </>
-  )
-}
+      `}</style>
+      </>
+  );
+};
 
 export default Footer;
